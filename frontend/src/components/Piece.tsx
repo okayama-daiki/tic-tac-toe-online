bd3e19408bc5f95220b80e9be826c058005b1ecd
dike-okayama <lawliet.d.k.m@gmail.com>
2023-09-27 12:22:32 +0900

    Add style props

--- frontend/src/components/Piece.tsx
+++ frontend/src/components/Piece.tsx
@@ -3,28 +3,27 @@ import * as styles from "./Piece.css";
 
 type PieceProps = {
   pieceType: CellState;
+  style?: React.CSSProperties;
 };
 
-const NOUGHT = (
-  <svg viewBox="0 0 128 128" className={styles.piece}>
-    <circle cx="64" cy="64" r="48" className={styles.nought}></circle>
-  </svg>
-);
+export default function Piece({ pieceType, style }: PieceProps) {
+  switch (pieceType) {
+    case CellState.NOUGHT:
+      return (
+        <svg viewBox="0 0 128 128" className={styles.piece} style={style}>
+          <circle cx="64" cy="64" r="48" className={styles.nought}></circle>
+        </svg>
+      );
 
-const CROSS = (
-  <svg viewBox="0 0 128 128" className={styles.piece}>
-    <path d="M16,16L112,112" className={styles.cross}></path>
-    <path d="M112,16L16,112" className={styles.cross}></path>
-  </svg>
-);
+    case CellState.CROSS:
+      return (
+        <svg viewBox="0 0 128 128" className={styles.piece} style={style}>
+          <path d="M16,16L112,112" className={styles.cross}></path>
+          <path d="M112,16L16,112" className={styles.cross}></path>
+        </svg>
+      );
 
-const EMPTY = <svg className={styles.piece}></svg>;
-
-export default function Piece({ pieceType }: PieceProps) {
-  const mapping = {
-    [CellState.NOUGHT]: NOUGHT,
-    [CellState.CROSS]: CROSS,
-    [CellState.EMPTY]: EMPTY,
-  };
-  return mapping[pieceType];
+    case CellState.EMPTY:
+      return <svg className={styles.piece} style={style}></svg>;
+  }
 }
