aa9923cef2d794411642b3fa8c93c9acbb8ea569
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-04-22 14:14:54 +0900

    Update base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tic-tac-toe-online/",
   plugins: [react(), vanillaExtractPlugin()],
 });
